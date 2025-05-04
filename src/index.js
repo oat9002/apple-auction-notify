c1199b9f63a33cf7f0763d4500452e9d1fe8ed4c
oat9002 <oat9002@hotmail.com>
2025-05-04 21:06:00 +0700

    ♻️ refactor car fetching logic to use wantedCars array

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import { getCarsDetailWithRetry } from "./appleAuction.js";
 import { sendMessage } from "./telegram.js";
 import { CronJob } from "cron";
 
+const wantedCars = [{
+  url: "https://www.appleauction.co.th/goods/Search?&ProductCategoryCode=MT&BrandCode=138&ModelCode=1380009&SubModelCode=&Year=&ColorCode=&GearCode=&CcId=&FuelType=&view=G&_=1746366403846",
+  mileage: 1606,
+  isMotorBike: true
+},
+{
+  url: "https://www.appleauction.co.th/goods/Search?&ProductCategoryCode=MT&BrandCode=138&ModelCode=1380007&SubModelCode=&Year=&ColorCode=&GearCode=&CcId=&FuelType=&view=G&_=1746366403849",
+  mileage: 4972,
+  isMotorBike: true
+},
+{
+  url: "https://www.appleauction.co.th/goods/Search?&ProductCategoryCode=MT&BrandCode=111&ModelCode=11105&SubModelCode=&Year=&ColorCode=&GearCode=&CcId=&FuelType=&view=G&_=1746366403850",
+  mileage: 6612,
+  isMotorBike: true
+}];
+
 console.log("Start processing...");
 
 if (process.env.RUN_ONCE === "true") {
@@ -47,19 +63,19 @@ function stopProcess(job) {
 }
 
 async function run() {
-  const data = await getCarsDetailWithRetry(
-    "https://www.appleauction.co.th/goods/Search?&ProductCategoryCode=CR&BrandCode=20&ModelCode=20007&SubModelCode=&Year=&ColorCode=&GearCode=&CcId=&FuelType=&view=G&_=1723195316008",
-  );
-  const wantedCar = data.find((carData) => carData.find((d) => d === "46,902"));
+  for (const car of wantedCars) {
+    const data = await getCarsDetailWithRetry(car.url);
+    const wantedCar = data.find((carData) => carData.find((d) => d.replace(",", "") === car.mileage.toString()));
 
-  if (wantedCar === undefined) {
-    console.log("No car found");
-  } else {
-    const carDetail = wantedCar.slice(1, wantedCar.length - 4).join(" ");
-    const auctionDate = (wantedCar[wantedCar.length - 1] !== "รอประมูล" ? wantedCar[wantedCar.length - 2] : "รอประมูล").replace(" ", "");
-    const auctionLocation = auctionDate !== "รอประมูล" ? wantedCar[wantedCar.length - 1] : "-";
-    const msg = `รายละเอียด: ${carDetail}\nวันประมูล: ${auctionDate}\nสถานที่: ${auctionLocation}`;
+    if (wantedCar === undefined) {
+      console.log("No car found");
+    } else {
+      const carDetail = car.isMotorBike ? wantedCar.slice(1, wantedCar.length - 1).join(" ") : wantedCar.slice(1, wantedCar.length - 4).join(" ");
+      const auctionDate = (wantedCar[wantedCar.length - 1] !== "รอประมูล" ? wantedCar[wantedCar.length - 2] : "รอประมูล").replace(" ", "");
+      const auctionLocation = auctionDate !== "รอประมูล" ? wantedCar[wantedCar.length - 1] : "-";
+      const msg = `รายละเอียด: ${carDetail}\nวันประมูล: ${auctionDate}\nสถานที่: ${auctionLocation}`;
 
-    sendMessage(msg);
+      sendMessage(msg);
+    }
   }
 }
